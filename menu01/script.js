37f2ae863fd41d1979bc36dbbfddb2bc13742089
summerflight <82556547+summerflight@users.noreply.github.com>
2021-04-20 10:03:27 +0900

    Add files via upload

---
+++ menu01/script.js
@@ -0,0 +1,21 @@
+var mapContainer = document.getElementById('wayto'). //지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(36.43237, 127.39426), //지도의 중심좌표
+    level: 3 //지도의 확대 레벨
+  };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성
+
+//마커가 표시될 위치
+var markerPosition = new kakao.maps.LatLng(36.43237, 127.39426);
+
+//마커 생성
+var marker = new kakao.maps.Marker ({
+  position: markerPosition
+});
+
+//마커가 지도 위에 표시되도록 설정
+marker.setMap(wayto);
+
+//아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);
