e86e136379e430af410fd3263d56d21f6ec56a82
summerflight <82556547+summerflight@users.noreply.github.com>
2021-04-20 11:25:53 +0900

    Add files via upload

--- menu01/script.js
+++ menu01/script.js
@@ -1,22 +1,21 @@
-var mapContainer = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var mapContainer = document.getElementById('map'). //지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(36.43237, 127.39426), //지도의 중심좌표
+    level: 3 //지도의 확대 레벨
+  };
 
-var mapOption = {
-  center: new window.kakao.maps.LatLng(36.43237, 127.39426), //지도의 중심좌표
-  level: 3 //지도의 확대 레벨
-}
-
-var map = new window.kakao.maps.Map(mapContainer, mapOption);
+var map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성
 
 //마커가 표시될 위치
-//var markerPosition = new kakao.maps.LatLng(36.43237, 127.39426);
+var markerPosition = new kakao.maps.LatLng(36.43237, 127.39426);
 
 //마커 생성
-//var marker = new kakao.maps.Marker ({
-//  position: markerPosition
-//});
+var marker = new kakao.maps.Marker ({
+  position: markerPosition
+});
 
 //마커가 지도 위에 표시되도록 설정
-//marker.setMap(wayto);
+marker.setMap(wayto);
 
 //아래 코드는 지도 위의 마커를 제거하는 코드입니다
 // marker.setMap(null);
