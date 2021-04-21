e8879b3e36fc638d6dc5e93a5c810377bd8024de
summerflight <82556547+summerflight@users.noreply.github.com>
2021-04-21 15:17:31 +0900

    210421

--- menu01/script.js
+++ menu01/script.js
@@ -1,31 +1,31 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
 // 지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+var map = new window.kakao.maps.Map(mapContainer, mapOption);
 
 // 주소-좌표 변환 객체를 생성합니다
-var geocoder = new kakao.maps.services.Geocoder();
+var geocoder = new window.kakao.maps.services.Geocoder();
 
 // 주소로 좌표를 검색합니다
 geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function(result, status) {
 
     // 정상적으로 검색이 완료됐으면
-     if (status === kakao.maps.services.Status.OK) {
+     if (status === window.kakao.maps.services.Status.OK) {
 
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
 
         // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
+        var marker = new window.kakao.maps.Marker({
             map: map,
             position: coords
         });
 
         // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
+        var infowindow = new window.kakao.maps.InfoWindow({
             content: '<div style="width:150px;text-align:center;padding:6px 0;">주식회사 더피치</div>'
         });
         infowindow.open(map, marker);
